73f961656eeddc37344e517084d9a462de755061
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-03-19 16:23:09 +0000

    Create blast_coi_id.sh

---
+++ blast_coi_id.sh
@@ -0,0 +1,16 @@
+#Bash script to COI if fasta sequences. Under development
+
+
+software="/software/team311/mu2/coi" #location of all scripts to run the COI-id pipeline
+database="/lustre/scratch116/vr/projects/vgp/user/mu2/coi-db/coi.updated.fa" #latest database for now
+FASTA="" #path to the fasta file just arriving from sequencing that you want to COI id.
+fasta_prefix=${FASTA#*2020/}
+
+seqtk sample $FASTA 40000 > $fasta_prefix.40000 #I'm subsampling the fasta file to have 40000 sequences. This is arbritary, can be changed. 
+blastn -query $fasta_prefix.40000 -db $database -evalue 1e-05 -out $fasta_prefix.40000.blastn
+wait
+perl $software/blastcov.pl $fasta_prefix.40000.blastn >  $fasta_prefix.40000.blastn.cov #parsing blast output. This is the same as blast format 8 output, with two extra colums that represent respectively the percentage of the query and subject in the blast match.
+wait
+cat $fasta_prefix.40000.blastn.cov | awk '{ if ($3>=97 && $14>=97) print $0}' >  $fasta_prefix.40000.blastn.cov.97
+
+echo "All done. Please check file <output>.97 to confirm the COI id for your fasta"
